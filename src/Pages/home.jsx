131574107d76c9cf2d899e83a85104334faed76e
Yannick Yanga <cleps2012@gmail.com>
2024-02-01 18:08:26 +0100

    small update

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,15 +1,14 @@
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import './home.css';
-
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 const PokemonListItem = ({ id, name, image, types }) => (
-  <div className='card'>
-    <img src={ image } alt={ name } />
-    <p>#{ id }</p>
-    <p>{ name }</p>
-    <ul className='type-container-home'>{ types }</ul>
+  <div className="card">
+    <img src={image} alt={name} />
+    <p>#{id}</p>
+    <p>{name}</p>
+    <ul className="type-container-home">{types}</ul>
   </div>
 );
 
@@ -17,7 +16,7 @@ PokemonListItem.propTypes = {
   id: PropTypes.number,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  types: PropTypes.string.isRequired
+  types: PropTypes.string.isRequired,
 };
 
 const Home = () => {
@@ -26,15 +25,16 @@ const Home = () => {
   useEffect(() => {
     const fetchPokemonData = async () => {
       try {
-        const promises = Array.from({ length: 1025 }, (_, index) =>
-          fetch(`https://pokeapi.co/api/v2/pokemon/${index + 1}/`)
-          .then((response) => response.json())
+        const promises = Array.from({ length: 200 }, (_, index) =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${index + 1}/`).then(
+            (response) => response.json()
+          )
         );
 
         const pokemonDataArray = await Promise.all(promises);
         setPokemonDataList(pokemonDataArray);
       } catch (error) {
-        console.error('Error fetching Pokémon data:', error);
+        console.error("Error fetching Pokémon data:", error);
       }
     };
 
@@ -42,24 +42,26 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='main'>
-      <div className='container'>
-        <h1 className='heading'>Pokedex</h1>
-        <span className='line'></span>
-        <div className='box'>
+    <div className="main">
+      <div className="container">
+        <h1 className="heading">Pokedex</h1>
+        <span className="line"></span>
+        <div className="box">
           {pokemonDataList.map((pokemon) => (
-          <div key={ pokemon.id } className='card'>
-            <Link to={`/pokemon/${pokemon.id}`}>
-              <PokemonListItem            
-                id={ pokemon.id }
-                name={ pokemon.name }
-                image={ pokemon.sprites.front_default }
-                types={ pokemon.types.map((type, index) => (
-                  <li key={ index } className='type-home'>{ type.type.name }</li>
-                ))}
-              />
-            </Link>
-          </div>
+            <div key={pokemon.id} className="card">
+              <Link to={`/pokemon/${pokemon.id}`}>
+                <PokemonListItem
+                  id={pokemon.id}
+                  name={pokemon.name}
+                  image={pokemon.sprites.front_default}
+                  types={pokemon.types.map((type, index) => (
+                    <li key={index} className="type-home">
+                      {type.type.name}
+                    </li>
+                  ))}
+                />
+              </Link>
+            </div>
           ))}
         </div>
       </div>
